05b6563d54a02b16f6d80e0b738177c6c6b504ab
pablopac <pavotar91@gmail.com>
2020-03-07 09:48:20 -0500

    Creacion base backup
    Se crea la herramienta para la creacion en backend

---
+++ backend/gestioningresoiue/src/test/java/com/gestioningresoiue/app/GestioningresoiueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioningresoiue.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioningresoiueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
