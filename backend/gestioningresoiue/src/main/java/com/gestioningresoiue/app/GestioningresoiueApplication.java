beef3815f87c05ae42e434fe4bb0f76da9d0a0d7
Juan Gabriel Perez Guerra <perezjuang@users.noreply.github.com>
2020-03-10 09:10:25 -0500

    Revert "Estructura Inicial - 07/03/2020"
    This reverts commit bf06127f76bd3789df36c3126bdeaf88651366ad.

---
+++ backend/gestioningresoiue/src/main/java/com/gestioningresoiue/app/GestioningresoiueApplication.java
@@ -0,0 +1,13 @@
+package com.gestioningresoiue.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioningresoiueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioningresoiueApplication.class, args);
+	}
+
+}
