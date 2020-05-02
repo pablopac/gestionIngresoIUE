653f1715d45eda723b93e95c89deba70d1ddc762
Juan Gabriel Perez Guerra <perezjuang@hotmail.com>
2020-05-02 08:35:36 -0500

    UpdateProyecto

---
+++ gestioningresoIUEBack/src/main/java/com/iue/GestioningresoIueApplication.java
@@ -0,0 +1,13 @@
+package com.iue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioningresoIueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioningresoIueApplication.class, args);
+	}
+
+}
