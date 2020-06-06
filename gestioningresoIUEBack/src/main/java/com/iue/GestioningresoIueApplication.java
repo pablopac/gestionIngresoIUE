43549be81376a55d33ccfc40f56320c45a7fe219
luisfferrer12 <44415835+luisfferrer12@users.noreply.github.com>
2020-06-06 09:32:31 -0500

    cambios para funcionar

--- gestioningresoIUEBack/src/main/java/com/iue/GestioningresoIueApplication.java
+++ gestioningresoIUEBack/src/main/java/com/iue/GestioningresoIueApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.app"})
+@ComponentScan(basePackages={"com.iue"})
 public class GestioningresoIueApplication {
 
 	public static void main(String[] args) {
